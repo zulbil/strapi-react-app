7e0a43eb33532ce05d4fdfc40f09adbb65e9198d
Zulbil <alexkhang25@yahoo.fr>
2021-09-14 15:09:49 +0100

    refactor in TestimonySectionHomepage

---
+++ frontend/src/components/TestimonySectionHomePage/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.scss'
+
+function TestimonySectionHomePage() {
+    return (
+        <section className="bg-light py-3 text-center testimony-section">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="card text-white bg-dark">
+                        <div className="card-body">
+                            <h4 className="card-title">Some quick example text to build on the card title and make up the bulk of the card's content.</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default TestimonySectionHomePage
