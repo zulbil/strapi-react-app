061fcb794b124c76c7de9bafd1dae0341a956491
Zulbil <alexkhang25@yahoo.fr>
2021-09-14 15:09:31 +0100

    correction in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,30 +9,26 @@ import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <main className="App main">
-        <div className="container-fluid" data-layout="container">
-          <div className="row">
-            <Navbar />
-            <Switch>
-                <Route path='/login'>
-                  <LoginPage />
-                </Route>
-                <Route path='/'>
-                  <HomePage />
-                </Route>
-                <Route path='/register'>
-                  <RegisterPage />
-                </Route>
-                <Route path='/journalists'>
-                  <JournalistsPage />
-                </Route>
-                <Route path='/agencies'>
-                  <AgenciesPage />
-                </Route>
-            </Switch>
-          </div>
-        </div>
-    </main>
+    <>
+      <Navbar />
+      <Switch>
+          <Route path='/login'>
+            <LoginPage />
+          </Route>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+          <Route path='/register'>
+            <RegisterPage />
+          </Route>
+          <Route path='/journalists'>
+            <JournalistsPage />
+          </Route>
+          <Route path='/agencies'>
+            <AgenciesPage />
+          </Route>
+      </Switch>
+    </>
   );
 }
 
