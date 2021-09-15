0b33bea3600e4492d771fc80ae2d6867ba0d8067
Zulbil <alexkhang25@yahoo.fr>
2021-09-15 16:25:36 +0100

    creating context

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthContextProvider({ children }) {
+
+    const [currentUser, setCurrentUser] = useState()
+
+    function signup(email,password) {
+        //TODO:: autheticate signup process
+    }
+
+    function login(email, password) {
+        //TODO:: authenticate signin process
+    }
+
+    const value = {
+        currentUser,
+        signup,
+        login
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
