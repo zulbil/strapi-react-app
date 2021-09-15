29fa7412998b0d01129b84432828bf717c9801bd
Zulbil <alexkhang25@yahoo.fr>
2021-09-15 11:09:38 +0100

    refactor authenticate route

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import LoginForm from '../components/LoginForm'
 
 function LoginPage() {
     return (
-        <div>
-            <h2>LOGIN PAGE</h2>
-        </div>
+        <main>
+            <div className="container-login-form">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-5">
+                            <LoginForm />
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </main>
     )
 }
 
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import RegisterForm from '../components/RegisterForm'
 
 function RegisterPage() {
     return (
-        <div>
-            <h2>REGISTER PAGE</h2>
-        </div>
+        <main>
+            <div className="container-login-form">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-5">
+                            <RegisterForm />
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </main>
     )
 }
 
