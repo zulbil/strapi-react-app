af57426c49a6e9b3e4f5be18fa35edf6b348e74b
Zulbil <alexkhang25@yahoo.fr>
2021-09-15 16:28:15 +0100

    build login functionnality

--- frontend/src/components/LoginForm/index.js
+++ frontend/src/components/LoginForm/index.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import './style.scss'
 
 const LoginForm = () => {
+
+    const emailRef                 =   useRef()
+    const passwordRef              =   useRef()
+
+    function handleLoginAction() {
+        alert('Login...')
+    }
+
     return (
-        <form className="shadow">
+        <form className="shadow needs-validation" noValidate onSubmit={handleLoginAction}>
             <div className="mb-3">
                 <label for="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <input type="email" className="form-control" id="exampleInputEmail1" ref={emailRef} aria-describedby="emailHelp" required/>
+                <div className="invalid-feedback">
+                    Please provide a valid email.
+                </div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+                <input type="password" className="form-control" id="exampleInputPassword1" ref={passwordRef} required />
+                <div className="invalid-feedback">
+                    Please provide a valid password.
+                </div>
             </div>
             <div className="d-grid gap-2">
                 <button type="submit" className="btn btn-block btn-warning fw-bold">Sign in</button>
