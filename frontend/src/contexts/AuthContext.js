d21cb8b373e265a327bc0c48c104d3fdcc1ddfb4
Zulbil <alexkhang25@yahoo.fr>
2022-02-10 03:01:43 -0600

    update signup and login function in AuthContext

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react"
+import axios from 'axios'
 
 const AuthContext = React.createContext()
 
@@ -10,12 +11,42 @@ export function AuthContextProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState()
 
-    function signup(email,password) {
-        //TODO:: autheticate signup process
+    function signup(username,email,password) {
+        axios
+            .post('http://localhost:1337/api/auth/local/register', {
+                username,
+                email,
+                password,
+            })
+            .then(response => {
+                // Handle success.
+                console.log('Well done!');
+                console.log(response);
+                console.log('User profile', response.data.user);
+                console.log('User token', response.data.jwt);
+            })
+            .catch(error => {
+                // Handle error.
+                console.log('An error occurred:', error.response);
+            });
     }
 
     function login(email, password) {
-        //TODO:: authenticate signin process
+        axios
+            .post('http://localhost:1337/api/auth/local', {
+                identifier: email,
+                password: password,
+            })
+            .then(response => {
+                // Handle success.
+                console.log('Well done!');
+                console.log('User profile', response.data.user);
+                console.log('User token', response.data.jwt);
+            })
+            .catch(error => {
+                // Handle error.
+                console.log('An error occurred:', error.response);
+            });
     }
 
     const value = {
